d0fcfb0ee7fb8c3885eba11fffed7482df8ed076
abdelouadoudborji <36171655+abdelouadoudborji@users.noreply.github.com>
2021-09-08 22:47:01 +0100

    GetHistorique

--- src/main/java/com/pantofit/workout/dao/WorkoutRepository.java
+++ src/main/java/com/pantofit/workout/dao/WorkoutRepository.java
@@ -13,6 +13,6 @@ import java.util.List;
 public interface WorkoutRepository extends JpaRepository<Workout,Long> {
 
 
-     ArrayList<Workout> findWorkoutByAbonnementID(Long AbonnementID);
+          ArrayList<Workout> findWorkoutByAbonnementID(Long AbonnementID);
 }
 
